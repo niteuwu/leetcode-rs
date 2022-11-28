90f462c00b234d1e5c842fd69f8f87d36b09bd83
Tanish Kushalkar <tanishkushalkar@gmail.com>
2022-11-28 23:40:49 +0530

    problem 13

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,41 @@
 fn main() {
-    println!("{}", is_pal(1221));
+    println!("{}", ri("MCMXCIV".to_string()));
 }
 
-fn is_pal(x: i32) -> bool {
-    let binding = x.to_string();
-    let s: String = binding.chars().rev().collect();
-    return x.to_string() == s;
+fn ri(s: String) -> i32 {
+    let mut x = 0;
+    let mut prev = 0;
+    let s: Vec<i32> = s.chars().map(|l| {
+        if l == 'I' { return 1; }
+        else if l == 'V' { return 5; }
+        else if l == 'X' { return 10; }
+        else if l == 'L' { return 50; }
+        else if l == 'C' { return 100; }
+        else if l == 'D' { return 500; }
+        else if l == 'M' { return 1000; }
+        0
+        
+        // if else if time efficient
+        // match is space efficient
+        // atleast in leetcode
+
+        // match l {
+        //     'I' => 1,
+        //     'V' => 5,
+        //     'X' => 10,
+        //     'L' => 50,
+        //     'C' => 100,
+        //     'D' => 500,
+        //     'M' => 1000,
+        //     _ => 0,
+        // }
+    }).collect();
+
+    for i in s {
+        if prev < i { x += i - 2 * prev; }
+        else { x += i; } 
+        prev = i
+    }
+
+    return x;
 }
